9710374509613a281e91e00009ebb6c14197bfb6
maha441997@gmail.com <maha441997@gmail.com>
2020-05-30 17:32:41 +0530

    generators

--- python-projects/Generators.py
+++ python-projects/Generators.py
@@ -1,62 +1,39 @@
-#Generator - range() yield pass
-#iterable -able to loop through
+#own for loop and range function
 
 def special_for(iterable):
-    iterator = iter(iterable)
+    iterator=iter(iterable)
     while True:
         try:
             print(iterator)
             print(next(iterator))
-            
+           
         except StopIteration:
             break
 
+
 special_for([1,2,3])
 
-def generator_function(num):
-    for i in range(num):   #range is function look like this
-        yield i*2
+
+#own range function
+
+class MyGen():
+    current=0
+    def __init__(self,first,last):
+        self.first=first
+        self.last=last
         
-g=generator_function(100)
-next(g)
-next(g)
-print(next(g))
-
-print(next(g))
-
-'''
-def make_list(number):
-    result=[]
-    for i in range(number):
-        result.append(i*2)
-    return result
-
-my_list=make_list(100)
-print(my_list)
-
-#print(list(range(1000000)))
-
-'''
-from time import time
-def performance(func):
-        def wrap(*args,**kwargs):
-               t1=time()
-               func(*args,**kwargs)
-               t2=time()
-               print(t2-t1)
-        return wrap
+    def __iter__(self):
+        return self
     
-@performance
-def long_time():
-    print('1')
-    for i in range(1000):
-        i*5
+    def __next__(self):
+        if MyGen.current<self.last:
+            number=MyGen.current
+            MyGen.current+=1
+            return number
         
-@performance
-def long_time2():
-    print('2')
-    for i in list(range(100)):
-        i*5
-        
-long_time()
-long_time2()
+        raise StopIteration
+
+gen=MyGen(0,100)
+
+for i in gen:
+    print(i)
